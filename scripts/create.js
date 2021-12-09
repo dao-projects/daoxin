74952cebb91eb90254427f8c14560a78e573ecc3
Your Name <you@example.com>
2021-12-09 18:19:02 +0800

    优化结构

---
+++ scripts/create.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: 创建项目工具类
+ * @version: 0.0.1
+ * @Author: liuhb
+ * @Date: 2021-12-09 18:11:20
+ * @LastEditors: liuhb
+ * @LastEditTime: 2021-12-09 18:14:34
+ */
+
+// 创建项目React脚手架
+console.log("创建React项目");
